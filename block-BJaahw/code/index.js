9b18e58c386abaae8343b967c78ad97af73185f4
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-08-04 02:58:09 +0530

    Class and Objects

--- block-BJaahw/code/index.js
+++ block-BJaahw/code/index.js
@@ -1,5 +1,13 @@
 // 1. Create a function `createUser` that accepts `name` and `age` and returns a new object with those properties.
-
+// function createUser(name, age) {
+//   let obj = {}
+//   obj.name = name;
+//   obj.age = age;
+//   obj.sayHello = function () {
+//     return `welcome ${obj.name}`
+//   }
+//   return obj
+// }
 // 2. Add a method named `sayHello` inside the object created above. When called it should alert `Welcome {user}`. Replace `{user}` with the name of the user.
 
 // 3. Use the data (name, age) of two different person to create the object using `createUser`. Store the returned value in `personOne` and `personTwo`.
@@ -17,3 +25,14 @@
 // 9. Check by creating two instance of the class using data of two different persons and re-assign the value of `personOne` and `personTwo`
 
 // 10. Try calling `personOne.sayHello()` and `personTwo.sayHello()`. Check if you get the required output.
+class CreateUser {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age
+  }
+  sayHello() {
+    return `welcome ${this.name}`
+  }
+}
+
+let person1 = new CreateUser(`nishi`, 25)
